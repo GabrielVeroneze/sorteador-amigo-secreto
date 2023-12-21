58d22c5166fdec0bf6f756128c12196c014f99da
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-21 19:44:32 -0300

    :sparkles: feat: Adiciona componente com as rotas

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/Theme'
 import ResetStyles from '@/styles/Reset'
 import GlobalStyles from '@/styles/Global'
+import AppRoutes from '@/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <ResetStyles />
             <GlobalStyles />
+            <AppRoutes />
         </ThemeProvider>
     </React.StrictMode>
 )
