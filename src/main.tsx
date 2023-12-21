a14fe83b58d65cfe79012dde50eaea4fb36e0f32
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-21 19:43:30 -0300

    :sparkles: feat: Adiciona tema, reset e estilos globais

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '@/styles/Theme'
+import ResetStyles from '@/styles/Reset'
+import GlobalStyles from '@/styles/Global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-
-	</React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <ResetStyles />
+            <GlobalStyles />
+        </ThemeProvider>
+    </React.StrictMode>
 )
