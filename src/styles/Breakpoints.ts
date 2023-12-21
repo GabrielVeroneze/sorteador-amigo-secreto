c3accb25fc6cfd530c4f74ea3cc068b2b9f891d1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-21 20:07:36 -0300

    :sparkles: feat: Define breakpoints para as media queries

---
+++ src/styles/Breakpoints.ts
@@ -0,0 +1,26 @@
+interface ISize {
+    xs: string
+    sm: string
+    md: string
+    lg: string
+    xl: string
+    xxl: string
+}
+
+const size: ISize = {
+    xs: '360px', // for small screen mobile
+    sm: '600px', // for mobile screen
+    md: '900px', // for tablets
+    lg: '1024px', // for laptops
+    xl: '1440px', // for desktop / monitors
+    xxl: '1920px', // for big screens
+}
+
+export const device = {
+    xs: `(max-width: ${size.xs})`,
+    sm: `(max-width: ${size.sm})`,
+    md: `(max-width: ${size.md})`,
+    lg: `(max-width: ${size.lg})`,
+    xl: `(max-width: ${size.xl})`,
+    xxl: `(max-width: ${size.xxl})`,
+}
