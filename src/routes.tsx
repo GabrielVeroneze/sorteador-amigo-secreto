22811bdb494d31f000285c9f42e15015c3248eb1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-21 19:48:07 -0300

    :sparkles: feat: Cria arquivo de gerenciamento de rotas

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
