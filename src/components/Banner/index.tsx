219bc5e61c3b7295e0e047ff89880daf74a60f3f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-22 18:21:23 -0300

    :sparkles: feat: Adiciona imagens responsivas

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,15 @@
 import { Container, Imagem, Logo } from './styles'
+import imagemSm from '@/assets/images/banner-figura.png'
+import imagemMd from '@/assets/images/banner-figura-1024.png'
 
 const Banner = () => {
     return (
         <Container>
             <Logo />
-            <Imagem />
+            <Imagem>
+                <source srcSet={imagemMd} media="(min-width: 1024px)" />
+                <img src={imagemSm} />
+            </Imagem>
         </Container>
     )
 }
