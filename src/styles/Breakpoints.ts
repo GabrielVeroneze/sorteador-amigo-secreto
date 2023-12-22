0e84b3a1b51a37c7596948a58c4c40a4ea8037da
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-22 16:21:02 -0300

    :bug: fix: Modifica para condição mínima

--- src/styles/Breakpoints.ts
+++ src/styles/Breakpoints.ts
@@ -17,10 +17,10 @@ const size: ISize = {
 }
 
 export const device = {
-    xs: `(max-width: ${size.xs})`,
-    sm: `(max-width: ${size.sm})`,
-    md: `(max-width: ${size.md})`,
-    lg: `(max-width: ${size.lg})`,
-    xl: `(max-width: ${size.xl})`,
-    xxl: `(max-width: ${size.xxl})`,
+    xs: `(min-width: ${size.xs})`,
+    sm: `(min-width: ${size.sm})`,
+    md: `(min-width: ${size.md})`,
+    lg: `(min-width: ${size.lg})`,
+    xl: `(min-width: ${size.xl})`,
+    xxl: `(min-width: ${size.xxl})`,
 }
