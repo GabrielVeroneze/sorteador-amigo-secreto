7241ccc7ca50602cceee0c3f2a1e96a32efde945
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-21 23:41:09 -0300

    :sparkles: feat: Adiciona componente de Banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Imagem, Logo } from './styles'
+
+const Banner = () => {
+    return (
+        <Container>
+            <Logo />
+            <Imagem />
+        </Container>
+    )
+}
+
+export default Banner
