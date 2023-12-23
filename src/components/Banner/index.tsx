efa8143bba40d50fd39edb38bbb21bb6f607bf75
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-22 22:28:54 -0300

    :truck: chore: Renomeia arquivos de imagem do banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,6 +1,6 @@
 import { Container, Imagem, Logo } from './styles'
-import imagemSm from '@/assets/images/banner-figura.png'
-import imagemMd from '@/assets/images/banner-figura-1024.png'
+import imagemSm from '@/assets/images/participante.png'
+import imagemMd from '@/assets/images/participante-1024.png'
 
 const Banner = () => {
     return (
