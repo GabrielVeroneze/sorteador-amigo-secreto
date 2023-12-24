520d634828557dd6246904c7601b5d1001246794
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-23 22:50:30 -0300

    :sparkles: feat: Adiciona as rotas da aplicação

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Layout from '@/pages/Layout'
+import Inicio from '@/pages/Inicio'
+import Sorteio from '@/pages/Sorteio'
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Inicio />} />
+                    <Route path="sorteio" element={<Sorteio />} />
+                </Route>
             </Routes>
         </Router>
     )
