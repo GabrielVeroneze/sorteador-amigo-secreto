d3c5789757b4dabe8fc985a4cf5849133d51b13f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-29 23:33:21 -0300

    :lipstick: feat: Modifica estilização para mobile first

--- src/components/Formulario/styles.ts
+++ src/components/Formulario/styles.ts
@@ -1,28 +1,32 @@
 import styled from 'styled-components'
 
 export const Campo = styled.form`
+    align-items: center;
     border-radius: 37.5px;
-    box-shadow: 4px 4px 0px 0px #000000;
     color: ${({ theme }) => theme.cores.texto.primaria};
     display: flex;
+    flex-direction: column;
     font-size: 1rem;
-    height: 82px;
+    gap: 0.5rem 0;
     position: relative;
 `
 
 export const Icone = styled.div`
     color: #0000004d;
-    left: 52px;
+    left: 26px;
     position: absolute;
-    top: 28px;
+    top: 12px;
 `
 
 export const Input = styled.input`
-    border: 2px solid #000000;
-    border-radius: 37.5px 0px 0px 37.5px;
-    flex-grow: 1;
+    border: 1px solid #000000;
+    border-radius: 37.5px;
+    box-shadow: 2px 2px 0px 0px #000000;
+    font-size: 0.875rem;
+    height: 48px;
     outline: none;
-    padding: 1.125rem 2.25rem 1.125rem 6.625rem;
+    padding: 0.875rem 1.625rem 0.875rem 4rem;
+    width: 100%;
 
     &::placeholder {
         color: #0000004d;
@@ -31,7 +35,10 @@ export const Input = styled.input`
 
 export const Botao = styled.button`
     background-color: ${({ theme }) => theme.cores.fundo.secundario};
-    border: 2px solid #000000;
-    border-radius: 0px 37.5px 37.5px 0px;
-    width: 227px;
+    border: 1px solid #000000;
+    border-radius: 37.5px;
+    box-shadow: 2px 2px 0px 0px #000000;
+    font-size: 1rem;
+    height: 48px;
+    width: 155px;
 `
