cbb5206609326b9b5f342c8412e32d4ba971ad56
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-29 23:58:00 -0300

    :lipstick: feat: Cria styled component para conteúdo principal

---
+++ src/pages/Layout/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Principal = styled.main`
+    background-color: ${({ theme }) => theme.cores.fundo.primaria};
+    border: 1px solid #000000;
+    border-radius: 48px 48px 0px 0px;
+    flex-grow: 1;
+`
