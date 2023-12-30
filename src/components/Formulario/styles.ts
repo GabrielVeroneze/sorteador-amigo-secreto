f1511a9b6d4e969d18f58b38d8a62c48bac19406
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-29 23:38:27 -0300

    :iphone: feat: Aplica responsividade

--- src/components/Formulario/styles.ts
+++ src/components/Formulario/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { device } from '@/styles/Breakpoints'
 
 export const Campo = styled.form`
     align-items: center;
@@ -9,6 +10,10 @@ export const Campo = styled.form`
     font-size: 1rem;
     gap: 0.5rem 0;
     position: relative;
+
+    @media ${device.lg} {
+        flex-direction: row;
+    }
 `
 
 export const Icone = styled.div`
@@ -16,6 +21,11 @@ export const Icone = styled.div`
     left: 26px;
     position: absolute;
     top: 12px;
+    
+    @media ${device.lg} {
+        left: 52px;
+        top: 28px;
+    }
 `
 
 export const Input = styled.input`
@@ -31,6 +41,15 @@ export const Input = styled.input`
     &::placeholder {
         color: #0000004d;
     }
+
+    @media ${device.lg} {
+        border: 2px solid #000000;
+        border-radius: 37.5px 0px 0px 37.5px;
+        box-shadow: 4px 4px 0px 0px #000000;
+        font-size: 1rem;
+        height: 82px;;
+        padding: 1.125rem 2.25rem 1.125rem 6.625rem;
+    }
 `
 
 export const Botao = styled.button`
@@ -41,4 +60,12 @@ export const Botao = styled.button`
     font-size: 1rem;
     height: 48px;
     width: 155px;
+
+    @media ${device.lg} {
+        border: 2px solid #000000;
+        border-radius: 0px 37.5px 37.5px 0px;
+        box-shadow: 4px 4px 0px 0px #000000;
+        height: 82px;
+        width: 227px;
+    }
 `
