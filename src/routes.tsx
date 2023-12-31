a473b920802172879854f066534a09a5be535b38
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-30 21:45:58 -0300

    :sparkles: feat: Envolve as rotas com <RecoilRoot>

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import Layout from '@/pages/Layout'
 import Inicio from '@/pages/Inicio'
 import Sorteio from '@/pages/Sorteio'
@@ -6,12 +7,14 @@ import Sorteio from '@/pages/Sorteio'
 const AppRoutes = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Inicio />} />
-                    <Route path="sorteio" element={<Sorteio />} />
-                </Route>
-            </Routes>
+            <RecoilRoot>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Inicio />} />
+                        <Route path="sorteio" element={<Sorteio />} />
+                    </Route>
+                </Routes>
+            </RecoilRoot>
         </Router>
     )
 }
