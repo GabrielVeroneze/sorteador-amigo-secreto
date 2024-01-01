abfc025b5e0db9b060eff8e76c6f72d16a75efd7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-31 23:39:38 -0300

    :sparkles: feat: Cria estado para manipular os dados do formulário

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,17 +1,31 @@
+import { useRef, useState } from 'react'
 import { Botao, Campo, Icone, Input } from './styles'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
 
 const Formulario = () => {
+    const [nome, setNome] = useState<string>('')
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const handleSubmit = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+
+        setNome('')
+        inputRef.current?.focus()
+    }
+
     return (
-        <Campo>
+        <Campo onSubmit={evento => handleSubmit(evento)}>
             <Icone>
                 <PersonAddIcon />
             </Icone>
             <Input
                 type="text"
                 placeholder="Insira os nomes dos participantes"
+                value={nome}
+                onChange={evento => setNome(evento.target.value)}
+                ref={inputRef}
             />
-            <Botao disabled>Adicionar</Botao>
+            <Botao disabled={!nome}>Adicionar</Botao>
         </Campo>
     )
 }
