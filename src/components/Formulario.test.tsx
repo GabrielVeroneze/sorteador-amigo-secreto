9f03803a6e3d97bd3c7bb43754798df9ce819c11
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-12-31 23:35:45 -0300

    :test_tube: test: Adiciona novo teste
    Adiciona teste para verificar a adição de um novo participante

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/Theme'
 import Formulario from './Formulario'
@@ -11,10 +12,31 @@ test('quando o input está vazio, novos participantes não podem ser adicionados
     )
 
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-
     const botao = screen.getByRole('button')
 
     expect(input).toBeInTheDocument()
-
     expect(botao).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <ThemeProvider theme={theme}>
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>
+        </ThemeProvider>
+    )
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const botao = screen.getByRole('button')
+
+    fireEvent.change(input, {
+        target: {
+            value: 'John'
+        }
+    })
+    fireEvent.click(botao)
+
+    expect(input).toHaveFocus()
+    expect(input).toHaveValue('')
 })
