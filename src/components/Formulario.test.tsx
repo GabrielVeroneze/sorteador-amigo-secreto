37ec0b1031c418f15918ab0062f9f44912588782
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-02 20:49:53 -0300

    :sparkles: feat: Utiliza describe para agrupar os testes relacionados

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -4,111 +4,113 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/Theme'
 import Formulario from '@/components/Formulario'
 
-test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
-    render(
-        <ThemeProvider theme={theme}>
-            <RecoilRoot>
-                <Formulario />
-            </RecoilRoot>
-        </ThemeProvider>
-    )
+describe('o comportamento do Formulario.tsx', () => {
+    test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+        render(
+            <ThemeProvider theme={theme}>
+                <RecoilRoot>
+                    <Formulario />
+                </RecoilRoot>
+            </ThemeProvider>
+        )
 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const botao = screen.getByRole('button')
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
 
-    expect(input).toBeInTheDocument()
-    expect(botao).toBeDisabled()
-})
-
-test('adicionar um participante caso exista um nome preenchido', () => {
-    render(
-        <ThemeProvider theme={theme}>
-            <RecoilRoot>
-                <Formulario />
-            </RecoilRoot>
-        </ThemeProvider>
-    )
-
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const botao = screen.getByRole('button')
-
-    fireEvent.change(input, {
-        target: {
-            value: 'John'
-        }
+        expect(input).toBeInTheDocument()
+        expect(botao).toBeDisabled()
     })
-    fireEvent.click(botao)
 
-    expect(input).toHaveFocus()
-    expect(input).toHaveValue('')
-})
+    test('adicionar um participante caso exista um nome preenchido', () => {
+        render(
+            <ThemeProvider theme={theme}>
+                <RecoilRoot>
+                    <Formulario />
+                </RecoilRoot>
+            </ThemeProvider>
+        )
 
-test('nomes duplicados não podem ser adicionados na lista', () => {
-    render(
-        <ThemeProvider theme={theme}>
-            <RecoilRoot>
-                <Formulario />
-            </RecoilRoot>
-        </ThemeProvider>
-    )
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const botao = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'John'
+            }
+        })
+        fireEvent.click(botao)
 
-    fireEvent.change(input, {
-        target: {
-            value: 'John'
-        }
+        expect(input).toHaveFocus()
+        expect(input).toHaveValue('')
     })
-    fireEvent.click(botao)
 
-    fireEvent.change(input, {
-        target: {
-            value: 'John'
-        }
+    test('nomes duplicados não podem ser adicionados na lista', () => {
+        render(
+            <ThemeProvider theme={theme}>
+                <RecoilRoot>
+                    <Formulario />
+                </RecoilRoot>
+            </ThemeProvider>
+        )
+
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
+
+        fireEvent.change(input, {
+            target: {
+                value: 'John'
+            }
+        })
+        fireEvent.click(botao)
+
+        fireEvent.change(input, {
+            target: {
+                value: 'John'
+            }
+        })
+        fireEvent.click(botao)
+
+        const mensagemDeErro = screen.getByRole('alert')
+
+        expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos!')
     })
-    fireEvent.click(botao)
     
-    const mensagemDeErro = screen.getByRole('alert')
+    test('a mensagem de erro deve sumir após os timers', () => {
+        jest.useFakeTimers()
 
-    expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos!')
-})
+        render(
+            <ThemeProvider theme={theme}>
+                <RecoilRoot>
+                    <Formulario />
+                </RecoilRoot>
+            </ThemeProvider>
+        )
 
-test('a mensagem de erro deve sumir após os timers', () => {
-    jest.useFakeTimers()
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
 
-    render(
-        <ThemeProvider theme={theme}>
-            <RecoilRoot>
-                <Formulario />
-            </RecoilRoot>
-        </ThemeProvider>
-    )
+        fireEvent.change(input, {
+            target: {
+                value: 'John'
+            }
+        })
+        fireEvent.click(botao)
 
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-    const botao = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'John'
+            }
+        })
+        fireEvent.click(botao)
 
-    fireEvent.change(input, {
-        target: {
-            value: 'John'
-        }
+        let mensagemDeErro = screen.queryByRole('alert')
+        expect(mensagemDeErro).toBeInTheDocument()
+
+        act(() => {
+            jest.runAllTimers()
+        })
+
+        mensagemDeErro = screen.queryByRole('alert')
+        expect(mensagemDeErro).toBeNull()
     })
-    fireEvent.click(botao)
-
-    fireEvent.change(input, {
-        target: {
-            value: 'John'
-        }
-    })
-    fireEvent.click(botao)
-
-    let mensagemDeErro = screen.queryByRole('alert')
-    expect(mensagemDeErro).toBeInTheDocument()
-
-    act(() => {
-        jest.runAllTimers()
-    })
-
-    mensagemDeErro = screen.queryByRole('alert')
-    expect(mensagemDeErro).toBeNull()
 })
