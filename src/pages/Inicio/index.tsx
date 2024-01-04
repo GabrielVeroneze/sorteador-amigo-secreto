33931317a22a572529b40282f7dbfa1944d09485
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-03 23:29:20 -0300

    :sparkles: feat: Adiciona lista com os nomes dos participantes
    Utiliza o hook customizado 'useListaDeParticipantes' para renderizar uma lista com os nomes dos participantes

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,8 +1,23 @@
+import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
+import { Lista, Participante } from './styles'
 import Formulario from '@/components/Formulario'
+import Botao from '@/components/Botao'
 
 const Inicio = () => {
+    const { listaDeParticipantes } = useListaDeParticipantes()
+
     return (
         <Formulario />
+        <>
+            <Formulario />
+            <Lista>
+                {listaDeParticipantes.map((participante, index) => (
+                    <Participante key={index}>
+                        {participante}
+                    </Participante>
+                ))}
+            </Lista>
+        </>
     )
 }
 
