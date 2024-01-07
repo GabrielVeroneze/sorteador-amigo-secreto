158a3d9af29314c0ccbc05b3a49d003afdcb0721
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-06 23:05:15 -0300

    :test_tube: test: Adiciona novo teste
    Cria novo teste que verifica se o botão está desativado caso não exista participantes suficientes

---
+++ src/components/Rodape.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { RecoilRoot } from 'recoil'
+
+describe('onde não existem participantes suficientes', () => {
+    test('a brincadeira não pode ser iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        )
+
+        const botao = screen.getByRole('button')
+
+        expect(botao).toBeDisabled()
+    })
+})
