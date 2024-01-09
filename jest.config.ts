1bfcd46fe48f87cbf6898b7ff1b4940c85e9c59d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-08 22:15:31 -0300

    :wrench: chore: Configura o Jest

--- jest.config.ts
+++ jest.config.ts
@@ -7,5 +7,6 @@ export default {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(png|jpg|jpeg|gif|svg|ttf|eot)$': '<rootDir>/test/__ mocks __/fileMock.ts',
     },
 }
