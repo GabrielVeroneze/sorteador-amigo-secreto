9984120f27aabb508d947d4751aa4180fd9cd895
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-09 18:12:50 -0300

    :sparkles: feat: Envelopa componentes necessários no rodape

--- src/components/Rodape.test.tsx
+++ src/components/Rodape.test.tsx
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { RecoilRoot } from 'recoil'
+import { theme } from '@/styles/Theme'
 import Rodape from '@/components/Rodape'
 
 describe('onde não existem participantes suficientes', () => {
     test('a brincadeira não pode ser iniciada', () => {
         render(
-            <RecoilRoot>
-                <Rodape />
-            </RecoilRoot>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <RecoilRoot>
+                        <Rodape />
+                    </RecoilRoot>
+                </Router>
+            </ThemeProvider>
         )
 
         const botao = screen.getByRole('button')
