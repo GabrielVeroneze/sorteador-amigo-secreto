4c4795ecbd294e3652dc7f011037bd1f8f21a746
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-11 23:23:17 -0300

    :sparkles: feat: Adiciona menu de opções para os participantes

--- src/pages/Sorteio/index.tsx
+++ src/pages/Sorteio/index.tsx
@@ -1,6 +1,20 @@
+import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
+import { MenuOpcoes } from './styles'
+
 const Sorteio = () => {
+    const { listaDeParticipantes } = useListaDeParticipantes()
+
     return (
-        <div>Sorteio</div>
+        <>
+            <MenuOpcoes>
+                <option disabled selected hidden>
+                    Selecione seu nome
+                </option>
+                {listaDeParticipantes.map(participante => (
+                    <option key={participante}>{participante}</option>
+                ))}
+            </MenuOpcoes>
+        </>
     )
 }
 
