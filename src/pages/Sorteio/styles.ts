e42ca39861dac8b5e0c3295bc2e8ea01411cfcdc
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-12 16:35:44 -0300

    :iphone: feat: Aplica responsividade

--- src/pages/Sorteio/styles.ts
+++ src/pages/Sorteio/styles.ts
@@ -1,14 +1,23 @@
 import styled from 'styled-components'
+import { device } from '@/styles/Breakpoints'
 
 export const MenuOpcoes = styled.select`
     border: 1px solid #000000;
     border-radius: 32px;
     box-shadow: 2px 2px 0px 0px #000000;
+    color: ${({ theme }) => theme.cores.texto.primaria};
     font-family: ${({ theme }) => theme.fontes.familia.primaria};
     font-size: 1rem;
-    height: 56px;
+    height: 50px;
     line-height: normal;
     outline: none;
-    padding: 1rem 1.75rem;
+    padding: 0 1.75rem;
     width: 100%;
+
+    @media ${device.lg} {
+        border: 2px solid #000000;
+        box-shadow: 4px 4px 0px 0px #000000;
+        height: 75px;
+        padding: 0 2rem;
+    }
 `
