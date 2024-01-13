ba67c36a5319fe20ab6a469d144dd7378e3f5ed4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-12 23:16:30 -0300

    :sparkles: feat: Utiliza a função criada para realizar o sorteio

--- src/hooks/useSorteador.ts
+++ src/hooks/useSorteador.ts
@@ -1,12 +1,14 @@
 import { useSetRecoilState } from 'recoil'
 import { resultadoAmigoSecreto } from '@/state/atom'
 import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
+import { realizarSorteio } from '@/helpers/realizarSorteio'
 
 export const useSorteador = () => {
     const { listaDeParticipantes } = useListaDeParticipantes()
     const setResultado = useSetRecoilState(resultadoAmigoSecreto)
 
     const sortearParticipantes = () => {
+        const resultado = realizarSorteio(listaDeParticipantes)
 
         setResultado(resultado)
     }
