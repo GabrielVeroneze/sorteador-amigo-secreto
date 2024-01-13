14fda8a9c7ca89461fc64d8d4db568b2abe96b82
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-12 23:13:34 -0300

    :recycle: refactor: Extraí a regra de negócios para função pura

--- src/hooks/useSorteador.ts
+++ src/hooks/useSorteador.ts
@@ -1,27 +1,12 @@
 import { useSetRecoilState } from 'recoil'
 import { resultadoAmigoSecreto } from '@/state/atom'
 import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
-import shuffle from 'just-shuffle'
 
 export const useSorteador = () => {
     const { listaDeParticipantes } = useListaDeParticipantes()
     const setResultado = useSetRecoilState(resultadoAmigoSecreto)
 
     const sortearParticipantes = () => {
-        const totalDeParticipantes = listaDeParticipantes.length
-        const listaEmbaralhada = shuffle(listaDeParticipantes)
-
-        const resultado = new Map<string, string>()
-
-        for (let index = 0; index < totalDeParticipantes; index++) {
-            const indiceAmigo =
-                index === totalDeParticipantes - 1 ? 0 : index + 1
-
-            resultado.set(
-                listaEmbaralhada[index],
-                listaEmbaralhada[indiceAmigo]
-            )
-        }
 
         setResultado(resultado)
     }
