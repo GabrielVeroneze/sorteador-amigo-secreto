a20f653eba9648ae324538f24bfd993a387ff78c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-13 23:55:11 -0300

    :test_tube: test: Adiciona novo teste
    Cria novo teste que verifica se o amigo secreto é exibido

--- src/pages/Sorteio.test.tsx
+++ src/pages/Sorteio.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import { RecoilRoot } from 'recoil'
 import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
@@ -28,5 +28,28 @@ describe('na página de sorteio', () => {
         const opcoes = screen.queryAllByRole('option')
 
         expect(opcoes).toHaveLength(listaDeParticipantes.length)
+    })
+
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(
+            <ThemeProvider theme={theme}>
+                <RecoilRoot>
+                    <Sorteio />
+                </RecoilRoot>
+            </ThemeProvider>
+        )
+
+        const select = screen.getByRole('combobox')
+        const botao = screen.getByRole('button')
+        const amigoSecreto = screen.getByRole('alert')
+
+        fireEvent.change(select, {
+            target: {
+                value: listaDeParticipantes[0]
+            }
+        })
+        fireEvent.click(botao)
+
+        expect(amigoSecreto).toBeInTheDocument()
     })
 })
