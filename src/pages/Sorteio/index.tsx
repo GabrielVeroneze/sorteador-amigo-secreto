67091874ee028b031c80d7d43f999c7e13d35f2c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-14 17:47:28 -0300

    :sparkles: feat: Adiciona botão para exibir o amigo secreto

--- src/pages/Sorteio/index.tsx
+++ src/pages/Sorteio/index.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
+import { useResultadoSorteio } from '@/hooks/useResultadoSorteio'
 import { MenuOpcoes } from './styles'
+import CasinoIcon from '@mui/icons-material/Casino'
+import Botao from '@/components/Botao'
 
 const Sorteio = () => {
     const { listaDeParticipantes } = useListaDeParticipantes()
+    const { resultadoSorteio } = useResultadoSorteio()
+
+    const [participanteAtual, setParticipanteAtual] = useState<string>('')
+    const [amigoSecreto, setAmigoSecreto] = useState<string | undefined>('')
+
+    const sortear = () => {
+        setAmigoSecreto(resultadoSorteio.get(participanteAtual))
+    }
 
     return (
         <>
-            <MenuOpcoes defaultValue="">
+            <MenuOpcoes
+                value={participanteAtual}
+                onChange={evento => setParticipanteAtual(evento.target.value)}
+                required
+            >
                 <option value="" disabled hidden>
                     Selecione seu nome
                 </option>
@@ -14,6 +30,11 @@ const Sorteio = () => {
                     <option key={participante}>{participante}</option>
                 ))}
             </MenuOpcoes>
+            {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
+            <Botao onClick={sortear}>
+                <CasinoIcon />
+                Sortear!
+            </Botao>
         </>
     )
 }
