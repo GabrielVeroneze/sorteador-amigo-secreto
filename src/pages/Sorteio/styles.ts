7cb8e0cadc8fd249eb4229419a6b228197381496
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-14 22:32:20 -0300

    :sparkles: feat: Cria styled components para página de sorteio

--- src/pages/Sorteio/styles.ts
+++ src/pages/Sorteio/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { device } from '@/styles/Breakpoints'
 
-export const MenuOpcoes = styled.select`
+export const Menu = styled.select`
     border: 1px solid #000000;
     border-radius: 32px;
     box-shadow: 2px 2px 0px 0px #000000;
@@ -19,5 +19,44 @@ export const MenuOpcoes = styled.select`
         box-shadow: 4px 4px 0px 0px #000000;
         height: 75px;
         padding: 0 2rem;
+    }
+`
+
+export const Texto = styled.p`
+    color: ${({ theme }) => theme.cores.texto.primaria};
+    font-size: 1.125rem;
+    line-height: normal;
+    margin: 2rem 0 1.375rem;
+    text-align: center;
+
+    @media ${device.lg} {
+        font-size: 1.25rem;
+        margin-bottom: 1.5rem;
+    }
+`
+
+export const TextoDestaque = styled.p`
+    color: ${({ theme }) => theme.cores.secundaria};
+    font-size: 1.125rem;
+    line-height: normal;
+    margin-top: 0.875rem;
+
+    @media ${device.lg} {
+        font-size: 1.625rem;
+        margin-top: 1rem;
+    }
+`
+
+export const Container = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+`
+
+export const Imagem = styled.picture`
+    margin-top: 1.25rem;
+
+    @media ${device.lg} {
+        margin-top: 2rem;
     }
 `
