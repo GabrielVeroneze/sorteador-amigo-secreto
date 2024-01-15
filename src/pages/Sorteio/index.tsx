200e012cefa1f54384d809861a4d45c5d273fc9b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-14 22:36:42 -0300

    :sparkles: feat: Finaliza página de sorteio

--- src/pages/Sorteio/index.tsx
+++ src/pages/Sorteio/index.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import { useListaDeParticipantes } from '@/hooks/useListaDeParticipantes'
 import { useResultadoSorteio } from '@/hooks/useResultadoSorteio'
-import { MenuOpcoes } from './styles'
+import { Container, Imagem, Menu, Texto, TextoDestaque } from './styles'
 import CasinoIcon from '@mui/icons-material/Casino'
 import Botao from '@/components/Botao'
+import Titulo from '@/components/Titulo'
+import imagemSm from '@/assets/images/aviao.png'
+import imagemMd from '@/assets/images/aviao-1024.png'
 
 const Sorteio = () => {
     const { listaDeParticipantes } = useListaDeParticipantes()
@@ -18,7 +21,8 @@ const Sorteio = () => {
 
     return (
         <>
-            <MenuOpcoes
+            <Titulo>Quem vai tirar o papelzinho?</Titulo>
+            <Menu
                 value={participanteAtual}
                 onChange={evento => setParticipanteAtual(evento.target.value)}
                 required
@@ -29,12 +33,23 @@ const Sorteio = () => {
                 {listaDeParticipantes.map(participante => (
                     <option key={participante}>{participante}</option>
                 ))}
-            </MenuOpcoes>
-            {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
-            <Botao onClick={sortear}>
-                <CasinoIcon />
-                Sortear!
-            </Botao>
+            </Menu>
+            <Texto>
+                Clique em em sortear para ver quem é seu amigo secreto!
+            </Texto>
+            <Container>
+                <Botao onClick={sortear}>
+                    <CasinoIcon />
+                    Sortear!
+                </Botao>
+                {amigoSecreto && (
+                    <TextoDestaque role="alert">{amigoSecreto}</TextoDestaque>
+                )}
+                <Imagem>
+                    <source srcSet={imagemMd} media="(min-width: 1024px)" />
+                    <img src={imagemSm} alt="Avião de papel" />
+                </Imagem>
+            </Container>
         </>
     )
 }
